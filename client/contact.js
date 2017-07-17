2b432fe165db16c817ab9daed4d8eb8e0199730a
AlexECA <achen2999@gmail.com>
2017-07-17 11:07:16 -0400

    Contact us

--- client/contact.js
+++ client/contact.js
@@ -1,7 +1,10 @@
 Template.contact.events({
-  "click .js-bugsubmit": function(event){
+  "click #js-bugsubmit": function(event){
     event.preventDefault();
     console.log("bug repported")
+    $(".alert").removeClass("in").show();
+    $(".alert").delay(400).addClass("in");
+    $('#js-yum').val('');
   },
 
 })
